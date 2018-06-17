ea57c43d5ba02ef6a8827955700ab13dd8ea3bf3
JLeo7 <jleomoram@gmail.com>
2018-06-17 00:07:23 -0600

    Primer ciclo. Clase Carta y su test.

---
+++ src/com/kata/Carta.java
@@ -0,0 +1,30 @@
+package com.kata;
+
+public class Carta {
+	
+	private String nombre;
+	private String palo;
+	private int valor;
+	
+	public Carta() {
+		
+	}
+	
+	public Carta(String nombre, String palo,int valor) {
+		this.nombre = nombre;
+		this.palo = palo;
+		this.valor = valor;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public String getPalo() {
+		return palo;
+	}
+	
+	public int getValor() {
+		return valor;
+	}
+}
---
+++ src/com/kata/test/CartaTest.java
@@ -0,0 +1,32 @@
+package com.kata.test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.kata.Carta;
+
+class CartaTest {
+	
+	@Test
+	public void testValorInvalido () throws Exception {
+		Carta cartaUno = new Carta("","",0);
+		Carta cartaDos = new Carta("","",11);
+		assertFalse(cartaUno.getValor() > 1 && cartaUno.getValor() < 10);
+		assertFalse(cartaDos.getValor() > 1 && cartaDos.getValor() < 10);
+	}
+	
+	@Test
+	public void testPalos () {
+		Carta cartaEscudo = new Carta("As","Escudos",1);
+		Carta cartaFlor = new Carta("As","Flores",1);
+		Carta cartaEstrella = new Carta("As","Estrellas",1);
+		Carta cartaGota = new Carta("As","Gotas",1);
+		assertEquals("Escudos",cartaEscudo.getPalo());
+		assertEquals("Flores",cartaFlor.getPalo());
+		assertEquals("Estrellas",cartaEstrella.getPalo());
+		assertEquals("Gotas",cartaGota.getPalo());
+	}
+
+}
